753ac2f25d21db6f35e5ec42302b6646fdb40b64
jason trieu <jltrieu@miners.utep.edu>
2025-03-22 23:20:41 -0600

    rudimentary redirects work

--- shell/shell.py
+++ shell/shell.py
@@ -8,32 +8,40 @@ import os, sys, re
 #  fd #2 is "standard error" (by default, attached to display for error output)
 
 PS1 = "$ "
+redir = re.compile(">")
 
 if 'PS1' in os.environ:
     PS1 = os.environ['PS1']
 
 while 1:
-    # we do input 
-    # look into env variables and PS1
     arg = input("%s" % PS1)
     argv = re.split(" ", arg) # for now, we just work with the first command
     print(argv)
 
     # INPUT PROCESSING
-    
+    if(argv[0] == "cd"):
+        os.chdir(argv[1])
+        print(os.getcwd()) 
+        continue
+    if(argv[0] == "exit"):
+        break
 
-    # first, fork
-    # then, make the parent wait and the child execute the program
     pid = os.getpid()
     rc = os.fork()
     
     if rc == 0:
-        if(argv[0] == "exit"):
-            sys.exit(0)
+        # does NOT support nested redirects; sorry!
+        redirIndex = ""
+        if redir.search(arg):
+            os.close(1) # goodbye stdout ! 
+            redirIndex = argv.index('>')
+            os.open(argv[redirIndex+1], os.O_CREAT | os.O_WRONLY) # hello new output!
+            os.set_inheritable(1, True)
         for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-            # os.write(1, ("%s/%s: trying\n" % (dir, argv[0])).encode())
             program = "%s/%s" % (dir, argv[0])
             try:
+                if(redirIndex):
+                    argv = argv[:redirIndex]
                 os.execve(program, argv, os.environ)
             except FileNotFoundError:
                 pass
@@ -44,6 +52,4 @@ while 1:
         sys.exit(1)
     else:
         childPidCode = os.wait()
-        if(argv[0] == "exit"): break
         continue
-
