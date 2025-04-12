680902aecdaea290898f6a0e8341347b52c51ed7
jason trieu <jltrieu@miners.utep.edu>
2025-04-12 06:40:23 -0600

    PIPING WORKSgit add shell.pygit add shell.py!

--- shell/shell.py
+++ shell/shell.py
@@ -39,32 +39,39 @@ while 1:
         background = True
     else: 
         background = False
-    # boolean for backgrounds
-    # if false, parent waits
-    # if true, dont wait
+
+
+    if(piper.search(arg)):
+        pipeIndex = argv.index('|')
+        argp = argv[pipeIndex+1:]
+        argv = argv[:pipeIndex]
+
+        pr,pw = os.pipe()
+        for f in (pr, pw):
+            os.set_inheritable(f, True)
+        print("pipe fds: pr=%d, pw=%d" % (pr, pw))
+    
     rc = os.fork()
     
     if rc == 0:
         # does NOT support nested redirects; sorry!
-        redirIndex = ""
+        if(background == True):
+            argv.pop()
         if redir.search(arg):
             os.close(1) # goodbye stdout ! 
             redirIndex = argv.index('>')
             os.open(argv[redirIndex+1], os.O_CREAT | os.O_WRONLY) # hello new output!
             os.set_inheritable(1, True)
+            argv = argv[:redirIndex]
         if piper.search(arg):
             os.close(1) 
-            pipeIndex = argv.index('|')
-            # do something else here
-            # 
-        if(background == True):
-            argv.pop()
+            os.dup2(pw, 1)
+            for fd in (pr, pw):
+                os.close(fd)
+
         for dir in re.split(":", os.environ['PATH']): # try each directory in the path
             program = "%s/%s" % (dir, argv[0])
             try:
-                if(redirIndex):
-                    argv = argv[:redirIndex]
-
                 os.execve(program, argv, os.environ)
             except FileNotFoundError:
                 pass
@@ -74,6 +81,28 @@ while 1:
         os.write(2, ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
     else:
+        if piper.search(arg):
+            rc = os.fork() # not this shit again
+            
+            if rc == 0:
+                os.close(0)
+                os.dup2(pr, 0)
+                for fd in (pr, pw):
+                    os.close(fd)
+                    
+                for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+                    program = "%s/%s" % (dir, argp[0])
+                    try:
+                        os.execve(program, argp, os.environ)
+                    except FileNotFoundError:
+                        pass
+            elif rc < 0: 
+                os.write(2, ("fork failed, returning %d\n" % rc).encode())
+                sys.exit(1)
+            
+            #os.write(0, os.read(pr, 9999)) # we have stdout at home 
+            for fd in (pr, pw):
+                os.close(fd)
         if(background == False):
             childPidCode = os.wait()
         continue
